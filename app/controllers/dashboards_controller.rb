2d0068a1b9350b99c787b8f184e6ddbbc9bcbcca
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-05 22:39:53 -0400

    Add all user shouts to dashboards#show template

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class DashboardsController < ApplicationController
   def show
     @text_subject = TextSubject.new
-    @timeline = current_user.timeline.page params[:page]
     @image_subject = ImageSubject.new
+    @timeline = current_user.timeline.page params[:page]
+    @all_shouts = Shout.all.order(created_at: :desc)
   end
 end
